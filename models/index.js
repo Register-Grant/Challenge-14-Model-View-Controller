9a2e96cbf1965a03424e2fc1d6cbceab17a31083
Grant Register <register.grant@gmail.com>
2022-08-10 14:57:53 -0600

    more model work

--- models/index.js
+++ models/index.js
@@ -1,3 +1,25 @@
 const User = require('./User');
 const BlogPost = require('./BlogPost');
 const Comment = require('./Comment');
+
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(BlogPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, BlogPost, Comment };
+

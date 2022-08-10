0be89c82daa0cb296e816bdedeabfec2a7a82865
Grant Register <register.grant@gmail.com>
2022-08-09 22:19:54 -0600

    manually filled in config and db and installed dependencies

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello stupid world'))
+//app.get('/', (req, res) => res.send('Hello stupid world'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(routes)
